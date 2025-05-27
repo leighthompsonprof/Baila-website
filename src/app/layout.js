2c3739099bf5ab5301902ef3ae3c32974d3c0c2e
Atticus Deutsch <afd3293@kellogg.local>
2025-05-27 16:47:56 -0500

    Removed unused MailerLite script

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,17 +17,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.variable}>
       <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-              .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-              n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-              (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-              ml('account', '1520366');
-            `
-          }}
-        />
       </head>
       <body
         className={`  font-sans antialiased bg-purple-200  min-h-screen flex flex-col`}
